351d0a1cf7f8314617e34a5c399e7d2740e69bdc
Haochen Liu <liuhaochen1221@hotmail.com>
2015-11-02 20:15:57 -0800

    Create class-contain-itself-6.cpp

---
+++ class-contain-itself-6.cpp
@@ -0,0 +1,48 @@
+/*
+这是无法编译通过的。
+pair是不支持的。
+*/
+
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <ctime>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <string>
+#include <stack>
+#include <sstream>
+#include <string.h>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+class ListNode {
+    int val;
+    pair<string, ListNode> p;
+};
+
+int main() {
+    ListNode node;
+    cout<<"test"<<endl;
+    
+    int wait = 0;
+    cin>>wait;
+
+    return 0; 
+}
